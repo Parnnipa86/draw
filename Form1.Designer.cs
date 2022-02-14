c3134de71be187fe45706fba45dbea4dc458eab3
พรรณนิภา ผลเจริญ <58881202+Parnnipa86@users.noreply.github.com>
2022-02-14 14:10:02 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDraw.Location = new System.Drawing.Point(201, 412);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(118, 41);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "วาดรูป";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(495, 412);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(118, 41);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "ลบทั้งหมด";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(357, 410);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(98, 43);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.Text = "สี";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 523);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace draw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Graphics myGraphic;
+            myGraphic = this.CreateGraphics();
+
+            Pen myPen1 = new Pen(System.Drawing.Color.LightGreen, 5);
+            Pen myPen2 = new Pen(System.Drawing.Color.Red, 5);
+            Pen myPen3 = new Pen(System.Drawing.Color.BlueViolet, 5);
+            Pen myPen4 = new Pen(System.Drawing.Color.Black, 5);
+            Pen myPen5 = new Pen(System.Drawing.Color.DarkRed, 5);
+
+            //วาดดาวโดยใช้DrawLine
+            //myGraphic.DrawLine(myPen1, 300, 80, 500, 80);
+            //myGraphic.DrawLine(myPen2, 300, 200, 500, 80);
+            //myGraphic.DrawLine(myPen3, 300, 80, 500, 200);
+            //myGraphic.DrawLine(myPen4, 300, 200, 400, 50);
+            //myGraphic.DrawLine(myPen5, 400, 50, 500, 200);
+
+            myGraphic.DrawRectangle(myPen1, 150, 100, 300, 200);
+            myGraphic.DrawRectangle(myPen2, 175, 125, 250, 150);
+            myGraphic.DrawRectangle(myPen3, 225, 150, 50, 50);
+            myGraphic.DrawRectangle(myPen3, 325, 150, 50, 50);
+            myGraphic.DrawRectangle(myPen4, 333, 183, 10, 10);
+            myGraphic.DrawRectangle(myPen4, 255, 158, 10, 10);
+            myGraphic.DrawRectangle(myPen5, 250, 230, 100, 30);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                btnColor.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
